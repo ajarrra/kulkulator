ef4defa316a0cf04fc1d1ff62a920f0441c7d262
ajarrra <ajarka1334@gmail.com>
2024-02-16 14:51:54 +0600

    finished my work

---
+++ js/core.js
@@ -0,0 +1,10 @@
+const inputLenght = document.querySelector('.range-input__1');
+const inputWidth = document.querySelector('.range-input__2');
+const inputHeight = document.querySelector('.range-input__3');
+
+
+const  inputLenghtValue = document.querySelector('.range-input_num__1');
+const  inputWidthValue = document.querySelector('.range-input_num__2');
+const  inputHeightValue = document.querySelector('.range-input_num__3');
+
+inpu
